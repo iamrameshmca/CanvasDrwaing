693debf869258d4b4773c4135524df88b1716ce4
iamrameshmca <67833846+iamrameshmca@users.noreply.github.com>
2020-07-13 22:21:14 +0800

    Create CanvasException.java

---
+++ src/main/java/exception/CanvasException.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class CanvasException extends RuntimeException{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -8306139561880992813L;
+
+	public CanvasException(String message) {
+        super(message);
+    }
+}
